b20fe161e545bbde56d4fa75cfb0a9866a31daf0
gzm <gaozhengming@sametoyou2020.com>
2023-05-25 16:27:29 +0800

    feat: 新增 v-viewer 插件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,33 @@
+import path from "path";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   typescript: {
-    shim: false
+    shim: false,
   },
-  modules: [
-    '@nuxt/image-edge',
-    '@nuxt/devtools'
-  ],
+  // 第三方模块
+  modules: ["@nuxt/image-edge", "@nuxt/devtools"],
   app: {
     head: {
-      style: [
-        { children: '* { margin: 0; padding: 0;}' }
-      ]
-    }
+      style: [{ children: "* { margin: 0; padding: 0;}" }],
+    },
   },
+  // 运行时配置
   runtimeConfig: {
     public: {
       BASE_URL: process.env.BASE_URL,
-    }
-  }
-})
+    },
+  },
+  // 第三方模块 [@nuxt/image-edge] 配置项
+  image: {
+    presets: {
+      avatar: {
+        modifiers: {
+          format: "jpg",
+          width: 32,
+          height: 32,
+        },
+      },
+    },
+  },
+});
